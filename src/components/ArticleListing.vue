2431209fa6d22baa0086b6f5c79734f34d0fc295
Mark Miller <mrkmiller@ucdavis.edu>
2017-10-11 16:16:07 -0700

    add related content to detail page:

---
+++ src/components/ArticleListing.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="vm-listing o-media">
+    <div class="o-media__figure" v-if="article.field_sf_primary_image">
+      <a :href="article.path.alias" class="category-brand__thumbnail"><img :src="article.field_sf_primary_image.url" :alt="article.title" /></a>
+    </div>
+    <div class="o-media__body">
+      <h3 class="vm-listing__title"><a :href="article.path.alias">{{ article.title }}</a></h3>
+      <div class="vm-listing__submitted">{{ submittedDate }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['article'],
+    computed: {
+      submittedDate () {
+        let date = new Date(this.article.created * 1000);
+        return (date.toLocaleString('en-us', { month: "long" })) + ' ' + date.getDate() + ', ' + date.getFullYear()
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .o-media__figure {
+    width: 135px;
+  }
+</style>
--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -19,17 +19,77 @@
       <li class="icon icon--location" v-if="person.field_sf_office_location">{{ person.field_sf_office_location }}</li>
       <li><address-display class="icon icon--map" v-if="person.field_sf_address" :address="person.field_sf_address"></address-display></li>
     </ul>
-    <strong>Office Hours</strong>
-    <p v-if="person.field_sf_office_hours">{{ person.field_sf_office_hours }}</p>
+    <template v-if="person.field_sf_office_hours">
+      <strong>Office Hours</strong>
+      <p >{{ person.field_sf_office_hours }}</p>
+    </template>
     <div v-if="person.body" v-html="person.body.value"></div>
+
+    <template v-if="files.length">
+      <h2 class="heading--auxiliary">Documents</h2>
+      <ul class="list--download">
+        <li v-for="file in files">
+          <a class="icon icon--link icon--download" :href="file.url">{{ file.filename }}</a>
+        </li>
+      </ul>
+    </template>
+
+    <template v-if="tags.length">
+      <h2 class="heading--auxiliary">Tags</h2>
+      <a :href="tag.path.alias" class="tags__tag" v-for="tag in tags">{{ tag.name }}</a>
+    </template>
+
+    <template v-if="articles.length">
+      <h2 class="heading--auxiliary">Related Articles</h2>
+      <article-listing v-for="article in articles" :article="article" :key="article.id"></article-listing>
+    </template>
+
   </section>
 </template>
 
 <script>
   import AddressDisplay from './AddressDisplay.vue'
+  import ArticleListing from './ArticleListing.vue'
+  import jsonapiParse from 'jsonapi-parse';
 
   export default {
-    components: { AddressDisplay },
-    props: ['person']
+    components: { AddressDisplay, ArticleListing },
+    data () {
+      return {
+        files: [],
+        tags: [],
+        articles: []
+      }
+    },
+    props: ['person'],
+    created () {
+      this.getReferencedContent();
+      this.getRelatedArticles()
+    },
+    methods: {
+      getReferencedContent () {
+        const url = 'http://sitefarm.acquia/jsonapi/node/sf_person/' + this.person.id + '?include=field_sf_files,field_sf_tags&fields%5Bnode--sf_person%5D=title,field_sf_files,field_sf_tags'
+
+        // Fetch the data via ajax
+        fetch(url)
+          .then(response => response.json())
+          .then(result => {
+            let data = jsonapiParse.parse(result).data;
+            this.files = data.field_sf_files;
+            this.tags = data.field_sf_tags;
+          });
+      },
+
+      getRelatedArticles () {
+        const url = 'http://sitefarm.acquia/jsonapi/node/sf_article?include=field_sf_person_reference,field_sf_primary_image&filter%5Bfield_sf_person_reference.uuid%5D%5Bvalue%5D=' + this.person.id;
+
+        // Fetch the data via ajax
+        fetch(url)
+          .then(response => response.json())
+          .then(result => {
+            this.articles = jsonapiParse.parse(result).data;
+          });
+      }
+    }
   }
 </script>
