f9926474518fd2749748dd29aaa2bbb25eaa2937
Mark Miller <mrkmiller@ucdavis.edu>
2017-10-11 12:58:17 -0700

    add search by name

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,17 @@
         <option value="person-listing">Listing</option>
         <option value="person-card">Card</option>
       </select>
+
+      <div class="search-form">
+        <label for="search-field" class="u-hidden--visually">Search</label>
+        <input type="input" placeholder="Search by Name..." id="search-field" class="search-form__input" v-model="nameFilter" @keyup.enter="filterList()">
+        <input type="image" class="search-form__submit" alt="Search" @click.prevent="filterList()">
+      </div>
+
     </div>
+    <div v-if="loading">Loading...</div>
     <component :is="currentView" v-for="person in people" :person="person" :key="person.id"></component>
+    <div v-if="!people.length" class="alert alert--error">Sorry! There are no people matching <strong>"{{ searchedValue }}"</strong></div>
   </div>
 </template>
 
@@ -24,7 +33,10 @@ export default {
   data () {
     return {
       people: [],
-      currentView: 'person-teaser'
+      loading: true,
+      currentView: 'person-teaser',
+      nameFilter: '',
+      searchedValue: ''
     }
   },
   created () {
@@ -32,13 +44,28 @@ export default {
   },
   methods: {
     getPeople () {
-      const url = 'http://sitefarm.acquia/jsonapi/node/sf_person?sort=field_sf_last_name&include=field_sf_primary_image';
+      const url = 'http://sitefarm.acquia/jsonapi/node/sf_person';
+      let params = '?sort=field_sf_last_name&include=field_sf_primary_image'
+
+      if (this.nameFilter) {
+        params += '&filter[title-filter][condition][path]=title'
+        params += '&filter[title-filter][condition][value]=' + this.nameFilter
+        params += '&filter[title-filter][condition][operator]=CONTAINS'
+      }
+
       // Fetch the data via ajax
-      fetch(url)
+      fetch(url + params)
         .then(response => response.json())
         .then(result => {
           this.people = jsonapiParse.parse(result).data;
+          this.loading = false;
       });
+    },
+
+    filterList () {
+      this.loading = true;
+      this.searchedValue = this.nameFilter;
+      this.getPeople();
     }
   }
 }
